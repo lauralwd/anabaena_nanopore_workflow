d83c9dc2a4c9ebb9c06d0d312b2639e7b4a9d50d
laura.lwd <laura.w.dijkhuizen@gmail.com>
2022-03-18 11:27:05 +0100

    implement bash-fu to create a fasta of deletions from sniffles vcf file

--- script.sh
+++ script.sh
@@ -317,23 +317,19 @@ do   count=$(echo "$r -1" | bc)      # correct for 0based counting
                   ) \
           > "$wd"/"$refname"_multi-sample_insertions.fasta
 
-#          # create a fasta file with the sequences of all deletions
-#          while read line
-#          do    name=$(echo $line | cut -d ' ' -f 1,2,3 | sed 's/\W/_/g')
-#                seq=$( echo $line | cut -f 5 -d ' ')
-#
-#                # check if seq is a seq, then print
-#                if                                          
-#                then echo \>"$name"
-#                     echo "$seq"
-#                fi
-#          done < <(grep -v '#' "$wd"/"$refname"_multi-sample.vcf \
-#                  | grep PASS \
-#                  | grep '\.DEL\.' \
-#                  ) \
-#          > "$wd"/"$refname"_multi-sample_deletions.vcf
-#          bedtools vcf to bed                              
-#          bed to fasta 				          
+          # create a fasta file with the sequences of all deletions
+          bedtools getfasta                                             \
+                   -fi "${refs[$count]}"                                \
+                   -fo "$wd"/"$refname"_multi-sample_deletions.fasta    \
+                   -bed <(                                              \
+                          grep -v '#' "$wd"/"$refname"_multi-sample.vcf \
+                          | grep PASS                                   \
+                          | grep '\.DEL\.'                              \
+                          | vcf2bed                                     \
+                          | cut -f 1,2,9                                \
+                          | sed 's/\t[PI].*;END=/\t/g'                  \
+                          | cut -d ';' -f 1                             \
+                          )
 
 #          # create a fasta file with the sequences of all translocations
 #          while read line
